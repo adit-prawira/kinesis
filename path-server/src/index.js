65e719627262642beb075328f5211c9214e0cdc4
Aditya Prawira <adityaprawira96@gmail.com>
2021-03-18 20:43:46 +1100

    Added setups for my-run-app

---
+++ path-server/src/index.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const mongoUri = process.env.DB_URL;
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected to Database");
+});
+
+mongoose.connection.on("error", () => {
+    console.error("Error connecting to mongo database");
+});
+app.get("/", (req, res) => {
+    res.send("hi");
+});
+
+app.listen(3000, () => {
+    console.log("Listening to port 3000");
+});
