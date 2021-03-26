138b4cc34b1a68df95c2dc236a13c67bc8792244
Aditya Prawira <adityaprawira96@gmail.com>
2021-03-26 20:42:59 +1100

    Added post routes and utilize authentification to create new track when user is logged in to their account

--- path-server/src/routes/trackRoutes.js
+++ path-server/src/routes/trackRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireAuth = require("../middlewares/requireAuth");
+const { route } = require("./authRoutes");
 const router = require("./authRoutes");
 const Track = mongoose.model("Track");
 
@@ -14,6 +15,34 @@ router.get("/tracks", async (req, res) => {
     const tracks = await Track.find({ userId: req.user._id });
 
     res.send(tracks);
+});
+
+router.post("/tracks", async (req, res) => {
+    const { name, locations } = req.body;
+
+    // user input invalid name or locations when creating a new track,
+    // then send invalid data status code with error custom messages
+    if (!name || !locations) {
+        return res.status(422).send({
+            error: "you must provide a name and locations",
+        });
+    }
+
+    // In the case there is an error (invalid data input) while creating a new track
+    // send invalid data status code
+    try {
+        const track = new Track({
+            name,
+            locations,
+            userId: req.user._id,
+        });
+
+        // save new track to the database
+        await track.save();
+        res.send(track);
+    } catch (e) {
+        res.status(422).send({ error: "Invalid locations" });
+    }
 });
 
 module.exports = router;
