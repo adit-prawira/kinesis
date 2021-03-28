0ce2aaa7fc5192344cf1ce13ce40e3e23f30f14d
Aditya Prawira <adityaprawira96@gmail.com>
2021-03-28 18:34:32 +1100

    added Spacer component and states to input email and password to sign up ]

---
+++ app/src/components/Spacer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    spacer: {
+        margin: 15,
+    },
+});
+
+const Spacer = ({ children }) => {
+    return <View style={styles.spacer}>{children}</View>;
+};
+
+export default Spacer;
--- app/src/screens/SignUpScreen.js
+++ app/src/screens/SignUpScreen.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 const styles = StyleSheet.create({
-    container: {},
-    button: {
-        padding: "5%",
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        marginBottom: 200,
     },
-    input: {
+    button: {
         padding: "5%",
     },
 });
 
 const SignUpScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <View style={styles.container}>
-            <Text
-                style={{
-                    fontSize: 48,
-                    textAlign: "center",
-                    marginBottom: "5%",
-                }}
-            >
-                Sign Up Screen
-            </Text>
-            <Input style={styles.input} label="Email" />
-            <Input style={styles.input} label="Password" />
+            <Spacer>
+                <Text
+                    style={{
+                        fontSize: 48,
+                        textAlign: "center",
+                    }}
+                >
+                    Sign Up to MyRun
+                </Text>
+            </Spacer>
+            <Spacer>
+                <Input
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    label="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+            </Spacer>
+            <Spacer>
+                <Input
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry
+                    label="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                />
+            </Spacer>
             <Button
                 title="Sign Up"
                 type="solid"
@@ -35,6 +57,12 @@ const SignUpScreen = ({ navigation }) => {
             />
         </View>
     );
+};
+
+SignUpScreen.navigationOptions = () => {
+    return {
+        header: null,
+    };
 };
 
 export default SignUpScreen;
